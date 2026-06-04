3aa29b8ec3510182e45239b5a716f5782f42aa19
Gitusertee <treasurerock6@gmail.com>
2026-06-03 23:36:25 -0400

    Fix typo in instance name tag for Jenk-server

--- terraform.tf
+++ terraform.tf
@@ -6,6 +6,6 @@ provider "aws"{
       ami           = "ami-0fe18bc3cfa53a248"
       instance_type = "t3.micro"
       tags = {
-          Name = "Jenk-server"
+          Name = "Jenk-serverr"
   }
    }
